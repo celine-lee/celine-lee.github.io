dc5516361187e721930b21b265304a1c1877d256
Celine Lee <celinelee@google.com>
2025-09-15 20:23:42 -0700

    distances

--- render_zines_page.py
+++ render_zines_page.py
@@ -7,6 +7,7 @@ THUMBNAIL_WIDTH = 800
 THUMBNAIL_HEIGHT = 800
 
 library_contents = [
+    ("https://medium.com/@celine.y.lee/distances-of-communication-d422a5d83eec", f"{thumbnail_folder}/distances_of_communication.jpg", "Distances of Communication"),
     ("https://medium.com/@celine.y.lee/lists-are-a-convenient-b-expressive-and-c-beloved-909eca72101b", f"{thumbnail_folder}/lists.jpg", "Lists"),
     ("https://medium.com/@celine.y.lee/bpe-hangman-ee8e72ff3987", f"{thumbnail_folder}/bpe_hangman.jpg", "BPE Hangman"),
     ("creatives/banana_store.pdf", f"{thumbnail_folder}/banana_store.jpg", "The Banana Store"),
